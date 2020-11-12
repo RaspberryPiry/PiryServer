3c59a6f742d651fbe81eb9cecea90f1c99c6e083
sshrik <chj8781942naver.com>
2020-11-12 22:44:32 +0900

    Add initial function.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var port = 36000;
 
 app.use(express.json());
 
-app.use('/user', require('./routes/user'));
+app.use('/list', require('./routes/list'));
+app.use('/picture', require('./routes/picture'));
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
---
+++ routes/list.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+var fs = require('fs');
+
+router.get('/all', (req, res, next) => {
+
+});
+
+router.get('/basic', (req, res, next) => {
+
+});
+
+router.get('/composite', (req, res, next) => {
+
+});
