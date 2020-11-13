58dae7477fcadf985c051ce0a7a53a132bd4a226
sshrik <chj8781942naver.com>
2020-11-13 22:51:09 +0900

    Change name pictures to load.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = 36000;
 app.use(express.json());
 
 app.use('/list', require('./routes/list'));
-app.use('/picture', require('./routes/picture'));
+app.use('/load', require('./routes/load'));
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
