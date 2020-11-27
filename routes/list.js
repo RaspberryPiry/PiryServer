d4d7a0bce45e65ac526d9dafd48d31460577316c
sshrik <chj8781942naver.com>
2020-11-28 00:15:17 +0900

    Weather API Add finished.

--- routes/list.js
+++ routes/list.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fileIO = require("../utils/fileIO");
 const listUtils = require("../utils/list");
 const config = require("../config");
+const { fstat } = require('fs');
 
 router.get('/all', (req, res, next) => {
     var fileList = listUtils.collectAllList()
@@ -35,8 +36,23 @@ router.get('/composite', (req, res, next) => {
 });
 
 router.get('/weather', (req, res, next) => {
-
+    fileIO.readJson(config.fileConfig.weather)
+    .then((data) => {
+        var weather = {
+            main : data.weather[0].main,
+            description : data.weather[0].description,
+            temp : data.main.temp - 273,
+            feels_like : data.main.feels_like - 273,
+            humidity : data.main.humidity,
+            windSpeed : data.wind.speed,
+            windDeg : data.wind.deg,
+            cloud : data.clouds.all,
+        }
+        res.json(weather);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
-
 
 module.exports = router;
